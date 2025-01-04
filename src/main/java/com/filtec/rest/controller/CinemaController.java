97f0d3ada0170068635e08962add81590a37a48a
Renato <renaato.d.m@gmail.com>
2025-01-04 15:20:24 -0300

    feat: documentation

--- src/main/java/com/filtec/rest/controller/CinemaController.java
+++ src/main/java/com/filtec/rest/controller/CinemaController.java
@@ -87,7 +87,7 @@ public class CinemaController {
     }
 
     @GetMapping(
-            value = "/movies/get-image/{movieId}",
+            value = "/movies/{movieId}/get-image",
             produces = MediaType.IMAGE_JPEG_VALUE
     )
     public @ResponseBody byte[] getMovieImage(@PathVariable Long movieId) {
